f23f85ad254d7e779f3bdfd473fa96c95d21148c
Aneesh <aneeshappid@gmail.com>
2025-02-10 00:48:39 +0530

    Started Home Page UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-      <p>FitGen</p>
-      <ul>
+    <div className='flex justify-between w-full p-10 px-15 bg-black'>
+      <p className='pl-4'>FitGen</p>
+      <ul className='flex gap-10'>
         <li>Home</li>
-        <li>H</li>
+        <li>Workouts</li>
         <li>About</li>
         <li>Contact</li>
       </ul>
+      <button>Join Now</button>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
+    <>
+  <Navbar />
     <div
-      className="h-screen flex items-center justify-center bg-cover bg-center relative"
+      className="h-screen items-center flex bg-cover bg-center relative"
       style={{ backgroundImage: "url('Home.png')" }}
     >
       {/* Overlay */}
       <div className="absolute inset-0 bg-opacity-50"></div>
 
       {/* Page */}
-      <div className="relative z-10 ">
-        
-       
+      <div className="relative z-10  w-full">
+       <div className='w-128 ml-20'>
+        <p className='text-7xl'>Elevate your <br />workout</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
+       </div>
       </div>
     </div>
+    </>
   )
 }
