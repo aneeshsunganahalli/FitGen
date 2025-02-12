30bad6820dfdf8e23d8efb4088b02100d7fb7157
Aneesh <aneeshappid@gmail.com>
2025-02-12 15:43:00 +0530

    Finished base home UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import {NavLink, useNavigate} from 'react-router-dom'
 
 export default function Navbar() {
+
+  const navigate = useNavigate()
+
   return (
-    <div className='flex justify-between w-full p-10 px-15 bg-black'>
-      <p className='pl-4'>FitGen</p>
-      <ul className='flex gap-10'>
-        <li>Home</li>
-        <li>Workouts</li>
-        <li>About</li>
-        <li>Contact</li>
+    <div className='flex items-center justify-between w-full p-10 px-15 bg-black'>
+      <p className='pl-4 text-2xl'>FitGen</p>
+      <ul className='flex gap-10 font-semibold text-gray-500'>
+        <NavLink to={'/'}><li>Home</li></NavLink>
+        <NavLink to={'/workouts'}><li>Workouts</li></NavLink>
+        <NavLink to={'/about'}><li>About</li></NavLink>
+        <NavLink to={'/contact'}><li>Contact</li></NavLink>
       </ul>
-      <button>Join Now</button>
+      <button onClick={() => navigate('/sign-up')} className='bg-white text-black rounded-lg py-3 px-6 font-semibold text-sm shrink-0 hover:opacity-85'>Join Now</button>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+
+  const navigate = useNavigate()
+
   return (
     <>
-  <Navbar />
-    <div
-      className="h-screen items-center flex bg-cover bg-center relative"
-      style={{ backgroundImage: "url('Home.png')" }}
-    >
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-opacity-50"></div>
+      <Navbar />
+      <div
+        className="h-screen flex bg-cover bg-center relative"
+        style={{ backgroundImage: "url('Home.png')" }}
+      >
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-opacity-50"></div>
 
-      {/* Page */}
-      <div className="relative z-10  w-full">
-       <div className='w-128 ml-20'>
-        <p className='text-7xl'>Elevate your <br />workout</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
-       </div>
+        {/* Page */}
+        <div className="relative z-10  w-full ">
+          <div className='w-128 ml-30'>
+
+            <p className='text-7xl font-semibold mt-50 mb-10'>Elevate your <br />workout</p>
+
+            <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
+
+            <div className=''>
+              <button onClick={() => navigate('/sign-up')} className='bg-white text-black text-sm font-semibold py-3 px-6 rounded-lg mt-15 hover:opacity-85'>Get started</button>
+
+              <div className='flex gap-5 mt-8'>
+                <img src="Twitter 1.png" alt="" />
+                <img src="Instagram.png" alt="" />
+                <img src="Facebook 1.png" alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
-    </div>
     </>
   )
 }
